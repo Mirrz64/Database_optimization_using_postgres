f5e8852da1e6f64c0f396601946a023d50f202fd
Miracle Osabuogbe <mosabuogbe@gmail.com>
2026-02-07 06:21:52 +0000

    Commit: Update rbac.sql

--- SQL/Security/rbac.sql
+++ SQL/Security/rbac.sql
@@ -30,3 +30,42 @@ create user Kolawole WITH PASSWORD 'Ace@123';  -- DA
 -- Grant permission to users based on their roles
 GRANT data_engineer_role TO Chinyere;
 Grant data_analyst_role TO Mohammed, KOLAWOLE;
+
+
+-- Assignment 1: Grant the Data Engineering Role Read-Only access to all tables in the research schema in the next
+GRANT SELECT ON ALL TABLES IN SCHEMA research TO data_engineer_role;
+
+-- Assignment 2: Grant the researcher role full read/write access to the research schema and read access to the drugs table.
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA research TO researcher_role;
+GRANT SELECT ON TABLE inventory.drugs TO data_engineer_role;
+
+-- Assignment 3: We just onboarded 4 new team members in LZ Pharma (Crainsor, Temitayo, Vincent, Anthony).
+-- Anthony was onboarded as a Database Administrator, Vincent was onboarded as a researcher,
+-- Temitayo was onboarded as a Business Analyst, and Crainsor was onboarded as a data engineer.
+-- Using the information given, create and grant user permissions based on their appropriate roles.
+
+CREATE ROLE database_admin_role NOLOGIN;
+CREATE ROLE business_analyst_role NOLOGIN;
+
+-- Database administrator permission
+GRANT ALL PRIVILEGES ON SCHEMA analytics, inventory, operations, research TO database_admin_role;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA analytics, inventory, operations, research TO database_admin_role;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA analytics, inventory, operations, research TO database_admin_role;
+
+-- Business analyst permission
+GRANT USAGE ON SCHEMA analytics, operations, inventory TO business_analyst_role;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA analytics TO business_analyst_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA operations, inventory TO business_analyst_role;
+
+-- Create the users
+CREATE USER Anthony WITH PASSWORD 'omk*#123';  -- DBA
+CREATE USER Vincent with password '*moh492#';  -- RES
+CREATE USER Temitayo WITH PASSWORD 'Ace@123';  -- BA
+CREATE USER Crainsor with password '*moh492#';  -- DE
+
+-- Grant permission to users based on their roles
+GRANT database_admin_role TO Anthony;
+Grant researcher_role TO Vincent;
+GRANT business_analyst_role TO Temitayo;
+Grant data_engineer_role TO Crainsor;
+
